bbe37d762b7525fac7939499cd98991e0972faa0
Mebabit <langwarmebabit@gmail.com>
2026-07-15 11:03:23 +0530

    I have hosted my index.html to github so I'm able to do request and I ahve fic everything like I don't have to copy path from local storage, I also learn that if we retrieve from local storage then request is not neccesary

--- ytFreeCodeCamp/timesjobs.py
+++ ytFreeCodeCamp/timesjobs.py
@@ -2,34 +2,33 @@ from bs4 import BeautifulSoup
 import requests
 import time
 
-print("Put some skill that you are not fimilar with")
+print("Put some skills that you are not fimilar")
 unfimilar_skill = input(">")
-print("Filtering out{unfimilar_skill}")
+print(f"Filtering out {unfimilar_skill}")
 
 def find_job():
-    html_text = requests.get("timesjob.html").text
+    html_text = requests.get("https://mebabit.github.io/forScraping/").text
     soup = BeautifulSoup(html_text, "lxml")
     jobs = soup.find_all("li", class_ = "clearfix job-bx wht-shd-bx")
-
     for index, job in enumerate(jobs):
         published_date = job.find("span", class_ = "sim-posted").span.text
         if "few" in published_date:
-            company_name = job.find("h3", class_ = "clearfix job-bx wht-shd-bx").text.replace(" ", "")
+            company_name = job.find("h3", class_ = "job-list-comp-name").text.replace(" ", "")
             skills = job.find("span", class_ = "srp-skills").text.replace(" ", "")
             more_info = job.header.h2.a["href"]
             if unfimilar_skill not in skills:
-                with open(f"posts/{index}.txt", "w") as f:
-                    
-                    time_wait(f"Company Name: {company_name.strip()}\n")
-                    time_wait(f"Skills Required: {skils.strip()}\n")
-                    time_wait(f"More Info: {more_info}")
-                print("File save: {index}")
+
+                with open(f"postsr/{index}.txt", "w") as f:
+                    f.write(f"Company name: {company_name}\n")
+                    f.write(f"Required Skills: {skills}\n")
+                    f.write(f"More Info: {more_info}")
+                print(f"File saved {index}")
 
 
 if __name__ == "__main__":
     while True:
-        find_jobs()
+        find_job()
         time_wait = 10
-        print(f"Waiting {time_wait} minutes...")
-        time.sleep(time_wait * 600)
+        print(f"Waiting {time_wait} minute...")
+        time.sleep(600)
         
